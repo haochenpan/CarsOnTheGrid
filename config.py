212f4d35e85d095388bb2038f17a3f7036b389db
HaochenPan <phchcc@gmail.com>
2019-01-17 14:43:10 -0500

    added plans ffor the future, based on 1/17's office discussion

--- config.py
+++ config.py
@@ -3,6 +3,16 @@
 """
 
 """
+    Plans for the future, not in particular order:
+        Display config info and round info on GUI
+        A chart shows how fast the message can reach every car in a round (x-round #; y-percentage)
+        Blind run, pick a few fast expanding cluster
+        User defined source position and route, will that accelerate message broadcasting?
+        Movement Model for all cars, how that will affect message broadcasting? Any new efficient strategies?
+        
+"""
+
+"""
     Definitions and terminologies (Version 01/11/2019, according to the email discussion):
         Source:         The first car holds the message, generate at the run time,
                         has id = FIRST_CAR_INDEX.
@@ -19,12 +29,12 @@
 NUM_OF_ROWS = 10
 NUM_OF_COLS = 10
 
-NUM_OF_CARS = 500
-NUM_OF_MOVES = 10
+NUM_OF_CARS = 1000
+NUM_OF_MOVES = 10  # TODO(Haochen): need more assertions on this
 
 # car movement:
 # [True|False] = [Can|Cannot] stay in the same block in two consecutive rounds
-ALLOW_STANDING = True
+ALLOW_STANDING = False
 
 """
     Common Configs - Preferences
@@ -43,7 +53,5 @@ FRAMES = 30
 """
     (Do not modify) Frequently Used Variables and Validations
 """
-
 LAST_ROW_INDEX = NUM_OF_ROWS + FIRST_ROW_INDEX - 1
 LAST_COL_INDEX = NUM_OF_COLS + FIRST_COL_INDEX - 1
-
