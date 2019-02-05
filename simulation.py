b85d50b66a2d46e81adb16badc82b5a88a7af53d
HaochenPan <phchcc@gmail.com>
2019-02-05 15:38:17 -0500

    cleaner Simulation Class

--- simulation.py
+++ simulation.py
@@ -1,6 +1,9 @@
 """
-    Multi-threading simulation doesn't increase the speed,
-    open several terminals and do multi-processing instead (for now)
+    Simulation: Simulate the game multiple times, store results to DB
+    Statistics: Query the DB to produce statistical results
+
+    Python's multi-threading DECREASES the speed,
+    so open several terminals to do multi-processing instead (for now)
 """
 from collections import defaultdict
 from threading import Thread, Event
@@ -13,21 +16,25 @@ from gui import Gui
 import matplotlib.pyplot as plt
 from pprint import PrettyPrinter
 
+COLLECTION_NAME = 'r20c20n1000r100-0205'
+
 
 class Simulation:
     def __init__(self):
-        self.on_going = Event()
+        self.on_going = Event()  # records user keyboard interruption
+        self.batch_vol = 50  # the number of records involves in a DB batch write
         self.data_queue = Queue()
-        self.batch_vol = 50
         self.simulator_num = 1
         self.db_writer_num = 1
-        self.simulation_ctr = 0  # a approx counter due to race condition
-        self.simulation_tme = time()
-        self.collection = MongoClient()['CarsOnTheGrid']['r20c20n1000r100-0201']
+        self.simulation_ctr = 0  # a approximate counter of records (due to race condition)
+        self.simulation_tme = time()  # the start time
+        self.collection = MongoClient()['CarsOnTheGrid'][COLLECTION_NAME]
 
     def _db_writer(self, name):
         write_list = []
-        while self.on_going.is_set():
+        # while the keyboard interruption has not been trigger OR
+        # there's some data that have not stored to DB:
+        while self.on_going.is_set() or not self.data_queue.empty():
             approx_qsize = self.data_queue.qsize()
             # print(f"{name} says approx_qsize is", approx_qsize)
             for i in range(self.batch_vol):
@@ -35,67 +42,56 @@ class Simulation:
                     write_list.append(self.data_queue.get_nowait())
                 except Empty:
                     break
-                except Exception as e:
-                    repr(e)
             if len(write_list) > 0:
-                # print(f"{name} is inserting")
                 self.collection.insert_many(write_list)
                 write_list = []
             sleep(0.5)
-        # print(f"{name} is exiting the main loop")
 
-        while not self.data_queue.empty():
-            for i in range(self.batch_vol):
-                try:
-                    write_list.append(self.data_queue.get_nowait())
-                except Empty:
-                    break
-                except Exception as e:
-                    repr(e)
-            if len(write_list) > 0:
-                # print(f"{name} is inserting")
-                self.collection.insert_many(write_list)
-                write_list = []
-        # print(f"{name} exited")
+        print(f"{name} exited")
 
     def _simulator(self, name):
         while self.on_going.is_set():
             g, s = main.run()
-            self.data_queue.put(s)
+            self.data_queue.put(s)  # can adjust whether the simulator stores the grid or not
             self.simulation_ctr += 1
-        # print(f"{name} exited")
+        print(f"{name} exited")
 
     def coordinate(self):
+        # the parent program initiates two kinds of threads
         self.on_going.set()
         threads = []
         for i in range(self.simulator_num):
             t = Thread(target=self._simulator, args=(f"Simulator - {i}",))
             t.start()
             threads.append(t)
-        print("Simulators booted")
+        print("Simulator(s) booted")
         for i in range(self.db_writer_num):
             w = Thread(target=self._db_writer, args=(f"Db-writer - {i}",))
             w.start()
             threads.append(w)
-        print("Db writers booted")
+        print("DB writer(s) booted")
+
+        # the parent program check threads' heartbeat and user's keyboard interruption
         try:
             while self.on_going.is_set():
-                for t in threads:
-                    if t.is_alive():
-                        print(f"Approx {self.simulation_ctr} items collected, "
-                              f"{self.simulation_ctr // (time() - self.simulation_tme)} items per second"
-                              f"data_queue size: {self.data_queue.qsize()}")
-                        sleep(5)
-                    else:
-                        print("something goes wrong")
-                        self.on_going.clear()
+                items_per_second = self.simulation_ctr // (time() - self.simulation_tme)
+                print(f"Approx {self.simulation_ctr} items collected, "
+                      f"{items_per_second} items per second, "
+                      f"approx data_queue size: {self.data_queue.qsize()}")
+                sleep(5)
         except KeyboardInterrupt as expected:
             self.on_going.clear()
-        for t in threads:
-            t.join()
+        finally:
+            for t in threads:
+                t.join()
         print("The coordinate exited")
         print(f"Approx {self.simulation_ctr} items collected, "
               f"at the speed of {self.simulation_ctr // (time() - self.simulation_tme)} items per second")
+
+
+class Statistics:
+    def __init__(self):
+        self.collection = MongoClient()['CarsOnTheGrid'][COLLECTION_NAME]
 
     def aggregate(self, filename):
         """
@@ -104,21 +100,21 @@ class Simulation:
         """
 
         # aggregate from the db
-        # stats = defaultdict(lambda: 0)
-        # docs = self.collection.find({}, {"stats": 1})
-        # for doc in docs:
-        #     stats[str(len(doc['stats']))] += 1
-        #
-        # with open('stats.pickle', 'wb') as handle:
-        #     pickle.dump(dict(stats), handle)
-        #     print("saved to pickle")
-        #
+        stats = defaultdict(lambda: 0)
+        docs = self.collection.find({}, {"stats": 1})
+        for doc in docs:
+            stats[str(len(doc['stats']))] += 1
+
+        with open('stats.pickle', 'wb') as handle:
+            pickle.dump(dict(stats), handle)
+            print("saved to pickle")
+
 
         # print aggregate dict
-        # with open('stats.pickle', 'rb') as handle:
-        #     stats = pickle.load(handle)
-        # for key in sorted(stats):
-        #     print(key, stats[key])
+        with open('stats.pickle', 'rb') as handle:
+            stats = pickle.load(handle)
+        for key in sorted(stats):
+            print(key, stats[key])
 
         # plot aggregate dict
         fig, ax = plt.subplots(figsize=(12, 12))
@@ -148,36 +144,20 @@ class Simulation:
         for i, e in enumerate(ids_list):
             print(i, e)
 
-    def db_to_pic(self):  # remember to change the dir and name where the fig store
-        with open('stats2.pickle', 'rb') as handle:
-            min_list = pickle.load(handle)
-        for i in range(0, len(min_list)):
-            curr_id = min_list[i]['_id']
-            record = self.collection.find_one({'_id': curr_id})
-            stats = {
-                "stats": [tuple(each) for each in record['stats']],
-                "trace": [(each[0], tuple(each[1])) for each in record['trace']],
-                "confi": record['confi']
-            }
-
-            Gui(data=(None, stats), mode=1, path=f'PhotoLibrary/Feb-50000/fig25/25-{i}-{curr_id}')
-            # Gui(data=(None, stats), mode=0)
-            print(i, " finished", len(min_list), "in total")
-
-
-class Statistics:
-    def __init__(self):
-        self.collection = MongoClient()['CarsOnTheGrid']['r20c20n1000r100-0201']
-
-    def record_to_pic(self, record):
+    @staticmethod
+    def record_to_pic(record):
         stats = {
             "stats": [tuple(each) for each in record['stats']],
             "trace": [(each[0], tuple(each[1])) for each in record['trace']],
             "confi": record['confi']
         }
 
+        # Gui(data=(None, stats), mode=1, path=f'PhotoLibrary/Feb-50000/fig25/25-{i}-{curr_id}')
         Gui(data=(None, stats), mode=1)
 
+        # print(i, " finished", len(min_list), "in total")
+
+    @staticmethod
     def record_to_sta(self, record):
         h_min, h_max = record['confi']['LAST_COL_INDEX'], record['confi']['FIRST_COL_INDEX']
         v_min, v_max = record['confi']['LAST_ROW_INDEX'], record['confi']['FIRST_ROW_INDEX']
@@ -349,11 +329,12 @@ class Statistics:
 
 
 if __name__ == '__main__':
+    pass
     # worker = Simulation()
     # worker.coordinate()
     # worker.aggregate("50000")
     # worker.find_ids()
     # worker.db_to_pic()
-    stats = Statistics()
+    # stats = Statistics()
     # stats.summarize_record()
-    stats.find_relation2()
+    # stats.find_relation2()
